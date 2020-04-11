f97f7b5ba6220191e71b8aacd24e6ccdb38424b2
Cherkashin Alexander <cherkashin_1996@mail.ru>
2020-04-11 17:02:28 +0300

    add stop command

--- src/extension.ts
+++ src/extension.ts
@@ -6,16 +6,30 @@ import { VirtualMachineTreeItem } from './vmTreeitem';
 export function activate(context: vscode.ExtensionContext) {
 	const vmProvider = new VirtualMachinesProvider();
 	vscode.window.registerTreeDataProvider("vb-machines", vmProvider);
+
 	context.subscriptions.push(
-		vscode.commands.registerCommand('virtualbox-extension.runVM', (vmTreeItem: VirtualMachineTreeItem) => {
-			const { vm } = vmTreeItem;
-			if (vm) {
+		vscode.commands.registerCommand('virtualbox-extension.runVM', (vmTreeItem?: VirtualMachineTreeItem) => {
+			if (vmTreeItem) {
+				const { vm } = vmTreeItem;
 				virtualbox.start(vm.id, true, (error) => {
 					vmProvider.refresh();
 					if (error) {
-						vscode.window.showErrorMessage(`Cannot run virtual machine"${vm.name}": ${error?.message ?? "Unknown error"}`);
+						vscode.window.showErrorMessage(`Cannot run virtual machine "${vm.name}": ${error?.message ?? "Unknown error"}`);
 					} else {
 						vscode.window.showInformationMessage(`Virtual machine "${vm.name}" has been run successfully`);
+					}
+				});
+			}
+		}),
+		vscode.commands.registerCommand("virtualbox-extension.stopVM", (vmTreeItem: VirtualMachineTreeItem) => {
+			if (vmTreeItem) {
+				const { vm } = vmTreeItem;
+				virtualbox.stop(vm.id, (error) => {
+					vmProvider.refresh();
+					if (error) {
+						vscode.window.showErrorMessage(`Cannot stop virtual machine "${vm.name}": ${error?.message ?? "Unknown error"}`);
+					} else {
+						vscode.window.showInformationMessage(`Virtual machine "${vm.name}" has been stopped successfully`);
 					}
 				});
 			}
