baf16ffede2b0710c22db3372d922b91e6dc98b3
Cherkashin Alexander <cherkashin_1996@mail.ru>
2020-04-11 15:34:25 +0300

    add refresh command

--- src/extension.ts
+++ src/extension.ts
@@ -6,20 +6,26 @@ import { VirtualMachine } from './utils';
 export function activate(context: vscode.ExtensionContext) {
 	const vmProvider = new VirtualMachinesProvider();
 	vscode.window.registerTreeDataProvider("vb-machines", vmProvider);
-	let disposable = vscode.commands.registerCommand('virtualbox-extension.runVM', (vm: VirtualMachine) => {
-		if (vm) {
-			virtualbox.start(vm.id, true, (error) => {
-				vmProvider.refresh();
-				if (error) {
-					vscode.window.showErrorMessage(`Cannot run virtual machine"${vm.name}": ${error?.message ?? "Unknown error"}`);
-				} else {
-					vscode.window.showInformationMessage(`Virtual machine "${vm.name}" has been run successfully`);
-				}
-			});
-		}
-	});
+	context.subscriptions.push(
+		vscode.commands.registerCommand('virtualbox-extension.runVM', (vm: VirtualMachine) => {
+			if (vm) {
+				virtualbox.start(vm.id, true, (error) => {
+					vmProvider.refresh();
+					if (error) {
+						vscode.window.showErrorMessage(`Cannot run virtual machine"${vm.name}": ${error?.message ?? "Unknown error"}`);
+					} else {
+						vscode.window.showInformationMessage(`Virtual machine "${vm.name}" has been run successfully`);
+					}
+				});
+			}
+		}),
+		vscode.commands.registerCommand('virtualbox-extension.refreshVms', () => {
+			vmProvider.refresh();
+		}),
+	);
 
-	context.subscriptions.push(disposable);
+
+
 }
 
 export function deactivate() { }
